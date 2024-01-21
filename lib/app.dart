adc8f91c4ce23ad17d4b4ea2859514a50036e1d9
mauro-balades <mauro.balades@tutanota.com>
2024-01-21 21:53:45 +0100

    Added Auth functionality

---
+++ lib/app.dart
@@ -0,0 +1,114 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:auth0_flutter/auth0_flutter_web.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'constants.dart';
+import 'hero.dart';
+import 'user.dart';
+
+class App extends StatefulWidget {
+  final Auth0? auth0;
+  const App({this.auth0, final Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  UserProfile? _user;
+
+  late Auth0 auth0;
+  late Auth0Web auth0Web;
+
+  @override
+  void initState() {
+    super.initState();
+    auth0 = widget.auth0 ??
+        Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+    auth0Web =
+        Auth0Web(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+
+    if (kIsWeb) {
+      auth0Web.onLoad().then((final credentials) => setState(() {
+            _user = credentials?.user;
+          }));
+    }
+  }
+
+  Future<void> login() async {
+    try {
+      if (kIsWeb) {
+        return auth0Web.loginWithRedirect(redirectUrl: 'http://localhost:3000');
+      }
+
+      var credentials = await auth0
+          .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+          .login();
+
+      setState(() {
+        _user = credentials.user;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      if (kIsWeb) {
+        await auth0Web.logout(returnToUrl: 'http://localhost:3000');
+      } else {
+        await auth0
+            .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+            .logout();
+        setState(() {
+          _user = null;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.only(
+          top: padding,
+          bottom: padding,
+          left: padding / 2,
+          right: padding / 2,
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Expanded(
+              child: Row(children: [
+            _user != null
+                ? Expanded(child: UserWidget(user: _user))
+                : const Expanded(child: HeroWidget())
+          ])),
+          _user != null
+              ? ElevatedButton(
+                  onPressed: logout,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  child: const Text('Logout'),
+                )
+              : ElevatedButton(
+                  onPressed: login,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  child: const Text('Login'),
+                )
+        ]),
+      )),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import auth0_flutter
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
